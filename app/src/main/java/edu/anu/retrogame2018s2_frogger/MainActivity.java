5de68b0a7085e59e9a617268aa94f76ff7deeb8a
Yang Yu <u6591743@anu.edu.au>
2018-09-30 01:10:27 +1000

    Initialize project.

---
+++ app/src/main/java/edu/anu/retrogame2018s2_frogger/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.anu.retrogame2018s2_frogger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

d84a07118d86d6ab0b314b4c534172ecc60e2a34
Jinwei Zhang <u6310407@anu.edu.au>
2018-10-01 15:57:05 +1000

    Implement FrogPaint class ,TextAline enum that defined in the UML.

---
+++ app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/FrogPaint.java
@@ -0,0 +1,40 @@
+package edu.anu.retrogame2018s2_frogger.frogger;
+
+public class FrogPaint {
+   private TextAlign textAlign;
+   private String color;
+   private int textSize;
+   private Boolean styleFill;
+
+    public TextAlign getTextAlign() {
+        return textAlign;
+    }
+
+    public void setTextAlign(TextAlign textAlign) {
+        this.textAlign = textAlign;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setTextSize(int textSize) {
+        this.textSize = textSize;
+    }
+
+    public int getTextSize() {
+        return textSize;
+    }
+
+    public void setStyleFill(Boolean styleFill) {
+        this.styleFill = styleFill;
+    }
+
+    public Boolean getStyleFill() {
+        return styleFill;
+    }
+}

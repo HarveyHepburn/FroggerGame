eb009db6c85ab9591a7103a9a6514135dafbb6fb
Yang Yu <u6591743@anu.edu.au>
2018-10-01 12:50:44 +1000

    Implemented FrogCanvas class

---
+++ app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/FrogCanvas.java
@@ -0,0 +1,9 @@
+package edu.anu.retrogame2018s2_frogger.frogger;
+
+public interface FrogCanvas {
+//    void drawRect(float left, float top, float right, float bottom, FrogPaint frogPaint);
+//    void drawCircle(float cx, float cy, float radius, FrogPaint frogPaint);
+//    void drawPath(Path path, FrogPaint frogPaint);
+//    void drawText(String text, float x, float y, FrogPaint frogPaint);
+//    void drawRoundRect(float left, float top, float right, float bottom, float rx, float ry, FrogPaint paint);
+}

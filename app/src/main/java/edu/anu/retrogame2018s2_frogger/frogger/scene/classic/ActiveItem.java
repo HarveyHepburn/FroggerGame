430955465454b19e7b3e07dde078266bc27c1fca
u6591743 <yyloveyv@gmail.com>
2018-10-05 18:15:29 +1000

    Finished ActiveItem class.

---
+++ app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/scene/classic/ActiveItem.java
@@ -0,0 +1,12 @@
+package edu.anu.retrogame2018s2_frogger.frogger.scene.classic;
+
+import edu.anu.retrogame2018s2_frogger.frogger.Drawable;
+import edu.anu.retrogame2018s2_frogger.frogger.Frog;
+
+interface ActiveItem extends Drawable {
+    int getX();
+    int getWidth();
+    boolean outOfMap();
+    boolean activity(Frog frog);
+    void resetItem(MovingParameters movingParameters);
+}

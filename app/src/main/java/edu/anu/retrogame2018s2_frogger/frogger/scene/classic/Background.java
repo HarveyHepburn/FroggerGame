f5907379c989a981606de9ef57778078cc9f9f1d
u6591743 <yyloveyv@gmail.com>
2018-10-05 19:36:46 +1000

    Finished Backgroud class.

---
+++ app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/scene/classic/Background.java
@@ -0,0 +1,19 @@
+package edu.anu.retrogame2018s2_frogger.frogger.scene.classic;
+
+
+import edu.anu.retrogame2018s2_frogger.frogger.FrogPaint;
+
+public class Background {
+//    private Scene scene;
+//    private FrogPaint frogPaint;
+//
+//    Background(Scene scene) {
+//        this.scene = scene;
+//        frogPaint = new FrogPaint();
+//        frogPaint.setColor("#90caf9");
+//    }
+//
+//    public void draw(FrogCanvas frogCanvas) {
+//        frogCanvas.drawRect(0, scene.getGameSetting().get("riverTop"), scene.getGameSetting().getWidth(), scene.getGameSetting().get("riverBottom"), frogPaint);
+//    }
+}

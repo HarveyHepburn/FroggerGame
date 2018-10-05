4fa478afcea25dd69bc1a4f3ff102e57f951d697
Yang Yu <u6591743@anu.edu.au>
2018-10-05 18:05:27 +1000

    Finished Animation class.

---
+++ app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/animation/Animation.java
@@ -0,0 +1,31 @@
+package edu.anu.retrogame2018s2_frogger.frogger.animation;
+
+public class Animation {
+    float speed;
+    float max;
+    float min;
+    boolean increasing = true;
+    float current = 0;
+
+    public Animation(float speed, float max, float min) {
+        this.speed = speed;
+        this.max = max;
+        this.min = min;
+    }
+
+    public void step() {
+        if (this.increasing) {
+            this.current += speed;
+            if (this.current >= this.max) {
+                this.current = max;
+                this.increasing = false;
+            }
+        } else {
+            this.current -= speed;
+            if (this.current <= this.min) {
+                this.current = min;
+                this.increasing = true;
+            }
+        }
+    }
+}

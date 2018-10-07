1a54e550710d659c8fd40856912d2ff69c6fe15d
Jinwei Zhang <u6310407@anu.edu.au>
2018-10-07 16:15:27 +1100

    change funtion name

--- app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/scene/classic/ItemManager.java
+++ app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/scene/classic/ItemManager.java
@@ -1,7 +1,7 @@
 package edu.anu.retrogame2018s2_frogger.frogger.scene.classic;
 
 public interface ItemManager {
-    public ActiveItem getActiveItem();
+    public ActiveItem getAvailableItem();
     public ActiveItem generateNewItem(MovingParameters movingParameters);
     public void addToPool(ActiveItem activeItem);
 }

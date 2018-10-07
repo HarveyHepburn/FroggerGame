72e8c0c1ff402a64ef649455afd4969a05f1aea6
Jinwei Zhang <u6310407@anu.edu.au>
2018-10-07 16:00:38 +1100

    add move

--- app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/scene/Scene.java
+++ app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/scene/Scene.java
@@ -1,5 +1,6 @@
 package edu.anu.retrogame2018s2_frogger.frogger.scene;
 
+import edu.anu.retrogame2018s2_frogger.frogger.Direction;
 import edu.anu.retrogame2018s2_frogger.frogger.FrogCanvas;
 import edu.anu.retrogame2018s2_frogger.frogger.SceneEndInformation;
 
@@ -7,7 +8,7 @@ public interface Scene {
     public void onEnterScene(FrogCanvas frogCanvas);
     public void onFinishScene(FrogCanvas frogCanvas);
     public void sceneOver(SceneEndInformation sceneEndInformation);
-  //  public void move(Direction direction);
+    public void move(Direction direction);
     public boolean isMoveble(int x,int y);//whether move is legel
     public Integer onTouch(float x,float y,boolean b);//b: is click down/up
     public boolean isActive();//whether scence is in active status

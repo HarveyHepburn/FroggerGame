2dd4d8210a153faaef9858cc6ab5a4e066a10202
dannyfeng <u6611178@anu.edu.au>
2018-10-09 10:32:58 +1100

    Push the previous finished work

--- app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/animation/FadeInAndOutAnimation.java
+++ app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/animation/FadeInAndOutAnimation.java
@@ -3,18 +3,29 @@ package edu.anu.retrogame2018s2_frogger.frogger.animation;
 import edu.anu.retrogame2018s2_frogger.frogger.FrogPaint;
 
 public class FadeInAndOutAnimation extends Animation {
-    boolean fadeIn;
 
-    public FadeInAndOutAnimation(float speed, float max, float min) {
-        super(speed, max, min);
+    boolean fadeIn = true;
+
+    public FadeInAndOutAnimation(int speed) {
+        super(speed, 255, 0);
     }
 
     @Override
     public void step() {
+        super.step();
 
     }
 
     public FrogPaint makePaint(FrogPaint frogPaint) {
+        if (frogPaint.getColor() == null) return frogPaint;
+        String color = frogPaint.getColor();
+        color = color.substring(1, color.length());
+        if (current < 16) {
+            color = "#0" + Integer.toHexString((int) current) + color;
+        } else {
+            color = "#" + Integer.toHexString((int) current) + color;
+        }
+        frogPaint.setColor(color);
         return frogPaint;
     }
 }

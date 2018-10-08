5703ccff6e3205e7e2b8d5918a3467c085f1dc47
dannyfeng <u6611178@anu.edu.au>
2018-10-09 10:15:02 +1100

    uncomment gamesetting

--- app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/scene/Scene.java
+++ app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/scene/Scene.java
@@ -3,15 +3,23 @@ package edu.anu.retrogame2018s2_frogger.frogger.scene;
 import edu.anu.retrogame2018s2_frogger.frogger.Direction;
 import edu.anu.retrogame2018s2_frogger.frogger.Drawable;
 import edu.anu.retrogame2018s2_frogger.frogger.FrogCanvas;
+import edu.anu.retrogame2018s2_frogger.frogger.GameSetting;
 import edu.anu.retrogame2018s2_frogger.frogger.SceneEndInformation;
 
 public interface Scene extends Drawable {
-    public void onEnterScene(FrogCanvas frogCanvas);
-    public void onFinishScene(FrogCanvas frogCanvas);
-    public void sceneOver(SceneEndInformation sceneEndInformation);
-    public void move(Direction direction);
-    public boolean isMoveble(int x,int y);//whether move is legel
-    public Integer onTouch(float x,float y,boolean b);//b: is click down/up
-    public boolean isActive();//whether scence is in active status
-    //public GameSetting getGameSetting();
+    void onEnterScene(FrogCanvas frogCanvas);
+
+    void onFinishScene(FrogCanvas frogCanvas);
+
+    void sceneOver(SceneEndInformation sceneEndInformation);
+
+    void move(Direction direction);
+
+    boolean isMoveble(int x, int y);//whether move is legel
+
+    Integer onTouch(float x, float y, boolean b);//b: is click down/up
+
+    boolean isActive();//whether scence is in active status
+
+    GameSetting getGameSetting();
 }
--- app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/scene/classic/FrogHole.java
+++ app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/scene/classic/FrogHole.java
@@ -1,55 +1,88 @@
 package edu.anu.retrogame2018s2_frogger.frogger.scene.classic;
 
-import android.transition.Scene;
-
 import java.util.Set;
+import java.util.HashSet;
 
 import edu.anu.retrogame2018s2_frogger.frogger.Drawable;
 import edu.anu.retrogame2018s2_frogger.frogger.Frog;
 import edu.anu.retrogame2018s2_frogger.frogger.FrogCanvas;
 import edu.anu.retrogame2018s2_frogger.frogger.FrogPaint;
+import edu.anu.retrogame2018s2_frogger.frogger.scene.Scene;
 
 public class FrogHole implements Drawable {
-    private FrogPaint frogPaint;
+
     FrogPaint frogPaintBg;
     FrogPaint frogPaintHole;
     FrogPaint frogPaintHoleIn;
     private Scene scene;
-    static final int HOLES_NUMBER = 4;
-    Set<Integer> inHoleNum;
-    int wallHeight;
-    int holeWidth;
+    int holeNumber = 1;
+    Set<Integer> inHoleNum = new HashSet<>();
+    int wallHeight = 100;
+    int holeWidth = 100;
 
     public FrogHole(Scene scene) {
         this.scene = scene;
-// this.add(new Lane(false,0,300,this,900,scene));
-        // I don't know the order of these parameters
+        this.frogPaintBg = new FrogPaint();
+        frogPaintBg.setColor("#3e2723");
+        frogPaintBg.setStyleFill(true);
+        frogPaintHole = new FrogPaint();
+        frogPaintHole.setColor("#ffffff");
+        frogPaintHole.setStyleFill(true);
+        frogPaintHoleIn = new FrogPaint();
+        frogPaintHoleIn.setColor("#33691e");
+        frogPaintHoleIn.setStyleFill(true);
+        this.holeNumber = scene.getGameSetting().get("holeNumber");
     }
 
     @Override
     public void draw(FrogCanvas frogCanvas) {
-        frogPaint = new FrogPaint();
-        frogPaint.setColor("#770618"); // land color
-        for (int i = 0; i < HOLES_NUMBER; i++) {
-//            float x = i * (scene.getGameSetting().getWidth()/HOLES_NUMBER) + 10;
-//            frogCanvas.drawCircle(x, getGameSetting().get("riverTop"), 10, frogPaint);
+        frogCanvas.drawRect(0, 0, scene.getGameSetting().getWidth(), wallHeight, frogPaintBg);
+        for (int i = 0; i < holeNumber; i++) {
+            int mid = (scene.getGameSetting().getWidth() / (holeNumber + 1)) * (i + 1);
+            if (inHoleNum.contains(i)) {
+                frogCanvas.drawCircle(mid, wallHeight, holeWidth, frogPaintHoleIn);
+            } else {
+                frogCanvas.drawCircle(mid, wallHeight, holeWidth, frogPaintHole);
+            }
         }
     }
 
     public boolean inHole(Frog frog) {
+        if (frog.getY() < wallHeight) {
+            for (int i = 0; i < holeNumber; i++) {
+                int mid = (scene.getGameSetting().getWidth() / (holeNumber + 1)) * (i + 1);
+                if (frog.getX() < mid + holeWidth / 2 && frog.getX() > mid - holeWidth / 2) {
+                    inHoleNum.add(i);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     public boolean isFinished() {
+        if (this.inHoleNum.size() >= holeNumber) {
+            return true;
+        }
         return false;
     }
 
-    public boolean isOkToMove(int a, int b) {
+    public boolean isOkToMove(int x, int y) {
+        if (y > wallHeight) {
+            return true;
+        } else {
+            for (int i = 0; i < holeNumber; i++) {
+                int mid = (scene.getGameSetting().getWidth() / (holeNumber + 1)) * (i + 1);
+                if (x < mid + holeWidth / 2 && x > mid - holeWidth / 2) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public void step() {
-        //Frog Hole doesn't move
+
     }
 }

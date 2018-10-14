69576cc50a19beffc55e0071ae8597681bc94900
u6310407 <u6310407@anu.edu.au>
2018-10-14 15:37:57 +1100

    change image

--- app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/scene/classic/Car.java
+++ app/src/main/java/edu/anu/retrogame2018s2_frogger/frogger/scene/classic/Car.java
@@ -1,15 +1,18 @@
 package edu.anu.retrogame2018s2_frogger.frogger.scene.classic;
 
 
+import java.util.Random;
+
 import edu.anu.retrogame2018s2_frogger.frogger.DialogInformation;
 import edu.anu.retrogame2018s2_frogger.frogger.Frog;
+import edu.anu.retrogame2018s2_frogger.frogger.FrogCanvas;
 import edu.anu.retrogame2018s2_frogger.frogger.scene.Scene;
 
 public class Car extends MovingItem implements ActiveItem {
     public Car(MovingParameters movingParameters, Scene scene) {
-        super(movingParameters,scene.getGameSetting());
+        super(movingParameters, scene.getGameSetting());
         frogPaint.setColor("#ff5252");
-        this.scene=scene;
+        this.scene = scene;
     }
 
     Scene scene;
@@ -18,6 +21,31 @@ public class Car extends MovingItem implements ActiveItem {
     public int getWidth() {
         return width;
     }
+
+    @Override
+    public void draw(FrogCanvas frogCanvas) {
+        Random random = new Random(1);
+        int res = random.nextInt(4);
+        String pic = "";
+        switch (res) {
+            case 0:
+                pic = "car_blue";
+                break;
+            case 1:
+                pic = "car_green";
+                break;
+            case 2:
+                pic = "car_red";
+                break;
+            case 3:
+                pic = "car_yellow";
+                break;
+
+
+        }
+        frogCanvas.drawImage(pic,x,mp.getY(),x+getWidth(),mp.getY()+height,frogPaint);
+    }
+
 
     @Override
     public boolean activity(Frog frog) {

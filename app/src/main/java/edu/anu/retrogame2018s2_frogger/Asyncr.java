a0e7e49fcaad3bede9ad7690abc391a9568a1b6f
u6310407 <u6310407@anu.edu.au>
2018-10-17 15:19:25 +1100

    add pic

---
+++ app/src/main/java/edu/anu/retrogame2018s2_frogger/Asyncr.java
@@ -0,0 +1,13 @@
+package edu.anu.retrogame2018s2_frogger;
+
+import android.os.AsyncTask;
+
+public class Asyncr extends AsyncTask {
+
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        this.execute();
+        return null;
+    }
+}

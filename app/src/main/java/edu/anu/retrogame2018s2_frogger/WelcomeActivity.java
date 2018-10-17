3101e8249ed838b555105b1e98785719795efc3e
u6310407 <u6310407@anu.edu.au>
2018-10-17 21:14:55 +1100

    add spinner

--- app/src/main/java/edu/anu/retrogame2018s2_frogger/WelcomeActivity.java
+++ app/src/main/java/edu/anu/retrogame2018s2_frogger/WelcomeActivity.java
@@ -7,16 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Spinner;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
 
 import edu.anu.retrogame2018s2_frogger.frogger.RecordInfo;
 import edu.anu.retrogame2018s2_frogger.frogger.player.DataProcess;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.prefs.Preferences;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Spinner spinner1, spinner2;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,87 +33,121 @@ public class WelcomeActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        DataProcess dataProcess = new DataProcess();
-        if (dataProcess.load().equals("")) {
-            setContentView(R.layout.activity_sign_up);
-        } else {
-            setContentView(R.layout.activity_welcome);
-            String res = dataProcess.load();
-            System.out.println(res);
-            TextView textView = (TextView) findViewById(R.id.toast);
-            textView.setText("Welcome " + res);
-        }
+        setContentView(R.layout.activity_welcome);
+        addItemsOnSpinner2();
+    }
+
+    public void addItemsOnSpinner2() {
+
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        List<String> list = new ArrayList<String>();
+        
+        list.add("list 1");
+        list.add("list 2");
+        list.add("list 3");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(dataAdapter);
     }
 
 
-    public void submit(View view) {
-        Intent intent = new Intent(this, WelcomeActivity.class);
-        startActivity(intent);
-        EditText editText = (EditText) findViewById(R.id.name);
-        String name = editText.getText().toString();
-        RankDatabaseHelper rankDatabaseHelper = new RankDatabaseHelper();
-        System.out.println(rankDatabaseHelper.playerExist(name));
-        if (rankDatabaseHelper.playerExist(name)) {
 
-            Toast.makeText(getApplicationContext(), "Already exits!Please change another one!", Toast.LENGTH_LONG).show();
-        } else if (name == "") {
-            Toast.makeText(getApplicationContext(), "Can't sign up! Please change another one!", Toast.LENGTH_LONG).show();
-        } else {
-            //add to database
-            rankDatabaseHelper.addData(new RecordInfo(name, -1, -1));//-1 means no player
-            DataProcess dataProcess = new DataProcess();
-            dataProcess.save(name);
-        }
+    // get the selected dropdown list value
+    public void addListenerOnButton() {
 
-    }
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
-    public void login(View view) {
-        Intent intent = new Intent(this, GameActivity.class);
-        startActivity(intent);
-        DataProcess dataProcess = new DataProcess();
-        String res = dataProcess.load();
-        TextView textView = (TextView) findViewById(R.id.toast);
-        textView.setText("Welcome " + res);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
 
-    }
+            @Override
+            public void onClick(View v) {
 
-    public void logout(View view) {
-        Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
-        DataProcess dataProcess = new DataProcess();
-        dataProcess.save("");
-    }
+                Toast.makeText(WelcomeActivity.this,
+                        "OnClickListener : " +
+                                "\nSpinner 1 : " + String.valueOf(spinner1.getSelectedItem()) +
+                                "\nSpinner 2 : " + String.valueOf(spinner2.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+            }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
+
+//                DataProcess dataProcess = new DataProcess();
+//
+//
+//
+//
+//                if (dataProcess.load().equals("")) {
+//            setContentView(R.layout.activity_sign_up);
+//        } else {
+//            setContentView(R.layout.activity_welcome);
+//            String res = dataProcess.load();
+//            System.out.println(res);
+//            TextView textView = (TextView) findViewById(R.id.toast);
+//            textView.setText("Welcome " + res);
+//        }
+//    }
+
+
+//    public void submit(View view) {
+//        Intent intent = new Intent(this, WelcomeActivity.class);
+//        startActivity(intent);
+//        EditText editText = (EditText) findViewById(R.id.name);
+//        String name = editText.getText().toString();
+//        RankDatabaseHelper rankDatabaseHelper = new RankDatabaseHelper();
+//       // System.out.println(rankDatabaseHelper.playerExist(name));
+//        if (rankDatabaseHelper.playerExist(name)) {
+//
+//            Toast.makeText(getApplicationContext(), "Already exits!Please change another one!", Toast.LENGTH_LONG).show();
+//        } else if (name == "") {
+//            Toast.makeText(getApplicationContext(), "Can't sign up! Please change another one!", Toast.LENGTH_LONG).show();
+//        } else {
+//            //add to database
+//            rankDatabaseHelper.addData(new RecordInfo(name, -1, -1));//-1 means no record
+//            DataProcess dataProcess = new DataProcess();
+//            dataProcess.save(name);
+//        }
+//
+//    }
+//
+//    public void login(View view) {
+//        Intent intent = new Intent(this, GameActivity.class);
+//        startActivity(intent);
+//        DataProcess dataProcess = new DataProcess();
+//        String res = dataProcess.load();
+//        TextView textView = (TextView) findViewById(R.id.toast);
+//        textView.setText("Welcome " + res);
+//
+//    }
+//
+//    public void logout(View view) {
+//        Intent intent = new Intent(this, SignUpActivity.class);
+//        startActivity(intent);
+//        DataProcess dataProcess = new DataProcess();
+//        dataProcess.save("");
+//    }
+
+//    @Override
+//    public void onWindowFocusChanged(boolean hasFocus) {
+//        super.onWindowFocusChanged(hasFocus);
+
+
 //        if (hasFocus) {
 //            hideSystemUI();
 //        }
+        });
+        }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//        Toast.makeText(parent.getContext(),
+//                "OnItemSelectedListener : " + parent.getItemAtPosition(pos).toString(),
+//                Toast.LENGTH_SHORT).show();
     }
 
-//    private void hideSystemUI() {
-//        // Enables sticky immersive mode.
-//        View decorView = getWindow().getDecorView();
-//        decorView.setSystemUiVisibility(
-//                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-//                        // Set the content to appear under the system bars so that the
-//                        // content doesn't resize when the system bars hide and show.
-//                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                        // Hide the nav bar and status bar
-//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
-//    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
-    // Shows the system bars by removing all the flags
-    // except for the ones that make the content appear under the system bars.
-//    private void showSystemUI() {
-//        View decorView = getWindow().getDecorView();
-//        decorView.setSystemUiVisibility(
-//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-//    }
+        // TODO Auto-generated method stub
+    }
 }
